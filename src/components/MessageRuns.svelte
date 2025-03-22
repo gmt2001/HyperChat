01341cedaffe557d7beabdfce6dcd5a0c6502771
gmt2001 <garymt2001@hotmail.com>
2025-03-21 21:24:28 -0400

    Add deleted runs

--- src/components/MessageRuns.svelte
+++ src/components/MessageRuns.svelte
@@ -7,6 +7,7 @@
   } from '../ts/storage';
 
   export let runs: Ytc.ParsedRun[] | null;
+  export let deletedRuns: Ytc.ParsedRun[] | null;
   export let forceDark = false;
   export let deleted = false;
   export let forceTLColor: Theme = Theme.YOUTUBE;
@@ -41,7 +42,7 @@
   >
     {#each runs as run}
       {#if run.type === 'text'}
-        {#if deleted}
+        {#if deleted && !deletedRuns?.length}
           <span>{run.text}</span>
         {:else}
           {#if run.styles?.includes('bold')}
@@ -52,6 +53,41 @@
             <TranslatedMessage text={run.text} {forceTLColor} />
           {/if}
         {/if}
+      {:else if run.type === 'link'}
+        <a
+          class="inline underline align-middle"
+          href={run.url}
+          target="_blank"
+        >
+          {run.text}
+        </a>
+      {:else if run.type === 'emoji' && $emojiRenderMode !== YoutubeEmojiRenderMode.HIDE_ALL}
+        {#if run.standardEmoji && $useSystemEmojis}
+          <span
+            class="cursor-auto align-middle text-base"
+          >
+            {run.alt}
+          </span>
+        {:else if run.src}
+          <img
+            class="h-5 w-5 inline mx-0.5 align-middle"
+            src={run.src}
+            alt={run.alt}
+            title={run.alt}
+          />
+        {/if}
+      {/if}
+    {/each}
+  </span>
+{/if}
+{#if deletedRuns?.length}
+  <span
+    class="cursor-auto align-middle {deletedClass} {$$props.class ?? ''}"
+    style="word-break: break-word"
+  >
+    {#each deletedRuns as run}
+      {#if run.type === 'text'}
+        <span>{run.text}</span>
       {:else if run.type === 'link'}
         <a
           class="inline underline align-middle"
