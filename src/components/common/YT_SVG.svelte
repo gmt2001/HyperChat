f742dd9c4a1d9a5355c07e65bae3245543eff2d2
gmt2001 <garymt2001@hotmail.com>
2025-04-14 01:37:33 -0400

    Implement YT_SVG icon

---
+++ src/components/common/YT_SVG.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let icon: string;
+  let path;
+  let fillrule = 'nonzero';
+  let classname = 'style-scope ytd-menu-service-item-renderer';
+  switch (icon) {
+    case 'WATCH_HISTORY':
+      path = 'M14.97 16.95 10 13.87V7h2v5.76l4.03 2.49-1.06 1.7zM22 12c0 5.51-4.49 10-10 10S2 17.51 2 12h1c0 4.96 4.04 9 9 9s9-4.04 9-9-4.04-9-9-9C8.81 3 5.92 4.64 4.28 7.38c-.11.18-.22.37-.31.56L3.94 8H8v1H1.96V3h1v4.74c.04-.09.07-.17.11-.25.11-.22.23-.42.35-.63C5.22 3.86 8.51 2 12 2c5.51 0 10 4.49 10 10z';
+      break;
+    case 'ACCOUNT_CIRCLE':
+      path = 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 1c4.96 0 9 4.04 9 9 0 1.42-.34 2.76-.93 3.96-1.53-1.72-3.98-2.89-7.38-3.03A3.996 3.996 0 0016 9c0-2.21-1.79-4-4-4S8 6.79 8 9c0 1.97 1.43 3.6 3.31 3.93-3.4.14-5.85 1.31-7.38 3.03C3.34 14.76 3 13.42 3 12c0-4.96 4.04-9 9-9zM9 9c0-1.65 1.35-3 3-3s3 1.35 3 3-1.35 3-3 3-3-1.35-3-3zm3 12c-3.16 0-5.94-1.64-7.55-4.12C6.01 14.93 8.61 13.9 12 13.9c3.39 0 5.99 1.03 7.55 2.98C17.94 19.36 15.16 21 12 21z';
+      classname = 'style-scope ytd-menu-navigation-item-renderer';
+      break;
+    case 'FLAG':
+      path = 'm13.18 4 .24 1.2.16.8H19v7h-5.18l-.24-1.2-.16-.8H6V4h7.18M14 3H5v18h1v-9h6.6l.4 2h7V5h-5.6L14 3z';
+      break;
+    case 'NOT_INTERESTED':
+      path = 'M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zM3 12c0 2.31.87 4.41 2.29 6L18 5.29C16.41 3.87 14.31 3 12 3c-4.97 0-9 4.03-9 9zm15.71-6L6 18.71C7.59 20.13 9.69 21 12 21c4.97 0 9-4.03 9-9 0-2.31-.87-4.41-2.29-6z';
+      fillrule = 'evenodd';
+      classname = 'style-scope ytd-menu-navigation-item-renderer';
+      break;
+    case 'DELETE':
+      path = 'M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z';
+      break;
+    case 'HOURGLASS':
+      path = 'M18 3h1V2H5v1h1v5.21L9.79 12 6 15.79V21H5v1h14v-1h-1v-5.21L14.21 12 18 8.21Zm-5.21 9L17 16.21V21H7v-4.79L11.21 12 7 7.79V3h10v4.79ZM12 16l4 4H8ZM9 7h6l-3 3Z';
+      break;
+    case 'REMOVE_CIRCLE':
+      path = 'M12 3c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9m0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2zm7 11H5v-2h14v2z';
+      break;
+    case 'ADD_CIRCLE':
+      path = 'M17 13h-4v4h-2v-4H7v-2h4V7h2v4h4v2zM12 3c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9m0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2z';
+      break;
+    default:
+      path = '';
+  }
+</script>
+
+<yt-icon class={classname}>
+  <span class="yt-icon-shape style-scope yt-icon yt-spec-icon-shape">
+    <div style="width: 100%; height: 100%; display: block; fill: currentcolor;">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;">
+      <path d={path} fill-rule={fillrule} />
+      </svg>
+    </div>
+  </span>
+</yt-icon>
