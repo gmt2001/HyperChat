733b8f0549f170fb6b2f1c6ebf1f4194d8582409
gmt2001 <garymt2001@hotmail.com>
2025-04-28 19:53:53 -0400

    Add Debug

--- src/ts/messaging.ts
+++ src/ts/messaging.ts
@@ -116,6 +116,7 @@ export const processSentMessage = (json: string): void => {
       }
     }
   };
+  console.log('processSentMessage', fakeChunk);
   interceptor.queue.addJsonToQueue(JSON.stringify(
     fakeChunk
   ), false, interceptor, true);
