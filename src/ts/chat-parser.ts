8adda4d225bb309835f975411f4dc0c569bb63d9
gmt2001 <garymt2001@hotmail.com>
2025-05-09 22:46:09 -0400

    Process bonks during initial load of live chat

--- src/ts/chat-parser.ts
+++ src/ts/chat-parser.ts
@@ -373,6 +373,8 @@ const processCommonAction = (
     return parseTickerAction(action.addLiveChatTickerItemAction, isReplay, liveTimeoutOrReplayMs);
   } else if (action.updateLiveChatPollAction) {
     return parsePollRenderer(action.updateLiveChatPollAction.pollToUpdate.pollRenderer);
+  } else if (action.markChatItemsByAuthorAsDeletedAction) {
+    return parseAuthorBonkedAction(action.markChatItemsByAuthorAsDeletedAction);
   }
 };
 
@@ -380,8 +382,6 @@ const processLiveAction = (action: Ytc.Action, isReplay: boolean, liveTimeoutMs:
   const common = processCommonAction(action, isReplay, liveTimeoutMs);
   if (common) {
     return common;
-  } else if (action.markChatItemsByAuthorAsDeletedAction) {
-    return parseAuthorBonkedAction(action.markChatItemsByAuthorAsDeletedAction);
   } else if (action.markChatItemAsDeletedAction) {
     return parseMessageDeletedAction(action.markChatItemAsDeletedAction);
   } else if (action.liveChatReportPresenceCommand) {
